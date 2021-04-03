271a9d8a449402054b189701de3d6698c2d992a7
leeenah <76856144+leeenah@users.noreply.github.com>
2021-04-02 17:48:23 -0700

    Used map() function to transform lists

--- src/Result.js
+++ src/Result.js
@@ -3,10 +3,19 @@ import React from "react";
 import "./Result.css";
 
 export default function Result(props) {
+  const listItems = props.result[0].meanings.map((meaning, index) => (
+    <div style={{ backgroundColor: "lightgrey" }} key={index}>
+      <p>{meaning.partOfSpeech}</p>
+      <p> {meaning.definitions[0].definition}</p>
+      <p>Example: {meaning.definitions[0].example}</p>
+    </div>
+  ));
+
   return (
     <div className="Result">
       <h3>{props.result[0].word}</h3>
       <div> {props.result[0].phonetics[0].text}</div>
+      <div>{listItems}</div>
     </div>
   );
 }
